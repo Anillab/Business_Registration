f99abc945271d84b34105ef378881b6c3ef92419
Anillab <anillabusolo@gmail.com>
2018-12-01 14:04:26 +0300

    travis

--- test_math.py
+++ test_math.py
@@ -3,8 +3,8 @@ import maths
 
 class TestMath(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(calc.add(2,4),6)
-        self.assertEqual(calc.add(-2,-4),-6)
-        self.assertEqual(calc.add(2,-4),-2)
+        self.assertEqual(maths.add(2,4),6)
+        self.assertEqual(maths.add(-2,-4),-6)
+        self.assertEqual(maths.add(2,-4),-2)
  if __name__ == '__main__':
      unittest.main()
