2936ff6ed25ecdec87bacc2d2116a9d1cf781c3f
Anillab <anillabusolo@gmail.com>
2018-12-08 13:09:06 +0300

    added a phone number field

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,15 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField,IntegerField
+from wtforms import StringField,PasswordField,SubmitField,BooleanField,IntegerField
 from wtforms.validators import DataRequired,Length,Email,EqualTo
 
 class SignUp(FlaskForm):
     username=StringField('Username',validators=[DataRequired(),Length(min=2,max=10)])
     email=StringField('Email',validators=[DataRequired(),Email()])
+    phone_number=IntegerField('Phone Number')
     password=PasswordField('Passsword',validators=[DataRequired()])
     password2=PasswordField('Confirm Passsword',validators=[DataRequired(),EqualTo('password')])
+
     submit=SubmitField('Sign Up')
+
 
 class SignIn(FlaskForm):
     email=StringField('Email',validators=[DataRequired(),Email()])
     password=PasswordField('Passsword',validators=[DataRequired()])
+    remember=BooleanField('Remeber Me')
     submit=SubmitField('Login')
